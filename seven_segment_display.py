379f5f5d6a9aa5c0e12c2bd70c55cb3ce93598f8
Mark Routledge <mark@mroutledge.co.uk>
2021-06-23 22:44:21 +0100

    Added, single digit combines 3 components to 7 segment string

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -1,3 +1,9 @@
 def digit(number):
     if number == 0:
         return [" _ ", "| |", "|_|"]
+
+
+def display_digit(number):
+    digit_components = digit(number)
+    seperator = "\n"
+    return seperator.join(digit_components)
--- seven_segment_display_test.py
+++ seven_segment_display_test.py
@@ -1,7 +1,12 @@
 import pytest
 
-from seven_segment_display import digit
+from seven_segment_display import digit, display_digit
 
 
 def test_digit():
     assert digit(0) == [" _ ", "| |", "|_|"]
+
+
+def test_display_single_digit_zero():
+    zero = " _ \n| |\n|_|"
+    assert display_digit(0) == zero
