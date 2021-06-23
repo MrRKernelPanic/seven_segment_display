9ce0cc5a40ac7746e64d481d58251b375cfdd204
Mark Routledge <mark@mroutledge.co.uk>
2021-06-24 00:15:55 +0100

    Encapsulated, digit into class

--- seven_segment_display.py
+++ seven_segment_display.py
@@ -1,20 +1,22 @@
-def digit(number):
-    number_dictionary = {
-        0: [" _ ", "| |", "|_|"],
-        1: ["   ", "  |", "  |"],
-        2: [" _ ", " _|", "|_ "],
-        3: [" _ ", " _|", " _|"],
-        4: ["   ", "|_|", "  |"],
-        5: [" _ ", "|_ ", " _|"],
-        6: [" _ ", "|_ ", "|_|"],
-        7: [" _ ", "  |", "  |"],
-        8: [" _ ", "|_|", "|_|"],
-        9: [" _ ", "|_|", "  |"]
-    }
-    return number_dictionary[number]
+class Digit:
 
+    def digit(self, number):
+        number_dictionary = {
+            0: [" _ ", "| |", "|_|"],
+            1: ["   ", "  |", "  |"],
+            2: [" _ ", " _|", "|_ "],
+            3: [" _ ", " _|", " _|"],
+            4: ["   ", "|_|", "  |"],
+            5: [" _ ", "|_ ", " _|"],
+            6: [" _ ", "|_ ", "|_|"],
+            7: [" _ ", "  |", "  |"],
+            8: [" _ ", "|_|", "|_|"],
+            9: [" _ ", "|_|", "  |"]
+        }
+        return number_dictionary[number]
 
-def display_digit(number):
-    digit_components = digit(number)
-    seperator = "\n"
-    return seperator.join(digit_components)
+    def display_digit(self, number):
+        digit_components = self.digit(number)
+        seperator = "\n"
+
+        return seperator.join(digit_components)
--- seven_segment_display_test.py
+++ seven_segment_display_test.py
@@ -1,24 +1,34 @@
 import pytest
 
-from seven_segment_display import digit, display_digit
+from seven_segment_display import Digit
+
+
+@pytest.fixture
+def digit():
+    return Digit()
 
 
 @pytest.mark.parametrize("number, components", [
-    (0, [' _ ', '| |', '|_|']),
-    (1, ['   ', '  |', '  |']),
-    (2, [' _ ', ' _|', '|_ ']),
-    (3, [' _ ', ' _|', ' _|']),
-    (4, ['   ', '|_|', '  |']),
-    (5, [' _ ', '|_ ', ' _|']),
-    (6, [' _ ', '|_ ', '|_|']),
-    (7, [' _ ', '  |', '  |']),
-    (8, [' _ ', '|_|', '|_|']),
-    (9, [' _ ', '|_|', '  |'])
+    (0, [" _ ", "| |", "|_|"]),
+    (1, ["   ", "  |", "  |"]),
+    (2, [" _ ", " _|", "|_ "]),
+    (3, [" _ ", " _|", " _|"]),
+    (4, ["   ", "|_|", "  |"]),
+    (5, [" _ ", "|_ ", " _|"]),
+    (6, [" _ ", "|_ ", "|_|"]),
+    (7, [" _ ", "  |", "  |"]),
+    (8, [" _ ", "|_|", "|_|"]),
+    (9, [" _ ", "|_|", "  |"])
 ])
-def test_digit_components(number, components):
-    assert digit(number) == components
+def test_digit_components(number, components, digit):
+    assert digit.digit(number) == components
 
 
-def test_display_single_digit_zero():
+def test_display_single_digit_zero(digit):
     zero = " _ \n| |\n|_|"
-    assert display_digit(0) == zero
+    assert digit.display_digit(0) == zero
+
+
+def test_display_single_digit_seven(digit):
+    seven = " _ \n  |\n  |"
+    assert digit.display_digit(7) == seven
