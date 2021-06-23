32c4d93ed9712054fa89237c24183169cab7ab3c
Mark Routledge <mark@mroutledge.co.uk>
2021-06-23 23:44:17 +0100

    Added, individual line components for all 9 digits.

--- seven_segment_display_test.py
+++ seven_segment_display_test.py
@@ -6,6 +6,14 @@ from seven_segment_display import digit, display_digit
 @pytest.mark.parametrize("number, components", [
     (0, [' _ ', '| |', '|_|']),
     (1, ['   ', '  |', '  |']),
+    (2, [' _ ', ' _|', '|_ ']),
+    (3, [' _ ', ' _|', ' _|']),
+    (4, ['   ', '|_|', '  |']),
+    (5, [' _ ', '|_ ', ' _|']),
+    (6, [' _ ', '|_ ', '|_|']),
+    (7, [' _ ', '  |', '  |']),
+    (8, [' _ ', '|_|', '|_|']),
+    (9, [' _ ', '|_|', '  |'])
 ])
 def test_digit_components(number, components):
     assert digit(number) == components
